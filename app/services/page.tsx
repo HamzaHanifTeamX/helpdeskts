20ff9e6cad1262e4215c29ce429bdb1c73273f94
HamzaHanifTeamX <hamzahanif@teamx.global>
2024-09-10 13:42:41 +0500

    query provider services update

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -26,7 +26,10 @@ const Page = () => {
         {data?.posts.map((post: any) => (
           <div key={post.id} className="mb-3 rounded-lg bg-slate-300 px-5 py-3 shadow">
             <h1 className="text-2xl font-bold text-slate-900">Title: <span className="text-slate-700">{post.title}</span></h1>
-            <p className="text-lg font-bold text-slate-600">Body: <span className="font-medium text-slate-400">{post.body}</span></p>
+            <p className="mb-2 text-lg font-bold text-slate-600">Body: <span className="font-medium text-slate-400">{post.body}</span></p>
+            <p className="text-base font-medium text-slate-400">Tags : <span className="flex flex-row items-center gap-4 text-slate-700">{post.tags.map((tag: any) => (
+              <span key={tag} className="rounded-lg bg-slate-700 px-2 py-1 text-white">{tag}</span>
+            ))}</span></p>
           </div>
         ))}
       </div>
